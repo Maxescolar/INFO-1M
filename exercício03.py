6c29a5cadc2bcc2a11c57fc4c107f58aee498106
Maxescolar <f.maxsuel@escolar.ifrn.edu.br>
2025-09-18 09:38:10 -0300

    Update exercício03.py

--- exercício03.py
+++ exercício03.py
@@ -2,5 +2,5 @@ nota01 = int(1)
 nota02 = int(2)
 nota03 = int(3)
 nota04 = int(10)
-
-print(f'{nota01 + nota02 + nota03 + nota04/4}')
+media = (nota01 + nota02 + nota03 + nota04)/4
+print(f'{media} ("sua media ai")')
