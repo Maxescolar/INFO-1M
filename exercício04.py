386ccbb2339611bb9a191a83014041d12509f51e
Maxescolar <f.maxsuel@escolar.ifrn.edu.br>
2025-09-18 09:45:58 -0300

    atividade boy
    mt massa tmj

---
+++ exercício04.py
@@ -0,0 +1,6 @@
+idade = int(input("qual sua idade?"))
+
+if idade >= 18:
+    print("É de maior visse")
+else:
+    print("auiahfyagfyugafyugafy")
---
+++ exercício05.py
@@ -0,0 +1,13 @@
+#notas de Allan
+nota01 = int(input("diga a primeira"))
+nota02 = int(input("diga a segunda"))
+nota03 = int(input("diga a tereceira"))
+nota04 = int(input("diga a quarta"))
+
+media = (nota01 + nota02 + nota03 + nota04)/4
+
+print(f'{media} ("tirou isso")')
+if media >= 6:
+    print("tu passou mlk chama")
+else:
+    print("tu n passou meus pêsames, oh mlk burro")
